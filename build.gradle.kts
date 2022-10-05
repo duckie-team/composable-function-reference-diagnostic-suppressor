39b958d7dbed9f37680b1f77e2544035485fa5e5
jisungbin <ji@sungb.in>
2022-10-06 00:27:38 +0900

    chore: bump compose compiler version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 
 dependencies {
     compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.7.20")
-    implementation("org.jetbrains.compose.compiler:compiler:1.2.0-alpha1-dev578")
+    implementation("org.jetbrains.compose.compiler:compiler:1.3.0-alpha01")
     compileOnly("com.google.auto.service:auto-service:1.0.1")
     kapt("com.google.auto.service:auto-service:1.0.1")
 }
